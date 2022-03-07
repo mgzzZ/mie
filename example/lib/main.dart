b8737f5e16ba58ade0207c56c6b675f867f40c30
zgm <zhangguoming@youzan.com>
2022-03-07 18:16:59 +0800

    feat:add radio

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:example/demos/buttons.dart';
 import 'package:example/demos/checkbox.dart';
 import 'package:example/demos/containers.dart';
+import 'package:example/demos/radio.dart';
 import 'package:example/demos/textfields.dart';
 import 'package:example/demos/texts.dart';
 import 'package:example/demos/tips.dart';
@@ -37,7 +38,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> data = ['Texts', 'Buttons', 'TextFields', 'Containers', 'Tips', 'CheckBox'];
+  List<String> data = ['Texts', 'Buttons', 'TextFields', 'Containers', 'Tips', 'CheckBox', 'Radio'];
 
   void pushPage(String name) {
     switch (name) {
@@ -58,6 +59,9 @@ class _MyHomePageState extends State<MyHomePage> {
         break;
       case 'CheckBox':
         Navigator.push(context, MaterialPageRoute(builder: (context) => MChcekBoxDemoPage()));
+        break;
+      case 'Radio':
+        Navigator.push(context, MaterialPageRoute(builder: (context) => MRadioDemoPage()));
         break;
       default:
     }
---
+++ lib/src/m_radio.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mie/src/radio_able.dart';
+
+///
+/// Created by zgm on 2022/3/7
+/// Describe:
+///
+
+class MRadio extends StatefulWidget {
+  const MRadio({Key? key, this.selected, required this.values, required this.onChanged})
+      : super(key: key);
+  final String? selected;
+  final List<String> values;
+  final ValueChanged<String> onChanged;
+  @override
+  _MRadioState createState() => _MRadioState();
+}
+
+class _MRadioState extends State<MRadio> with MRadioAble<String> {
+  String _value = '';
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.selected != null && widget.selected!.length != 0) {
+      initSelectedAcrion(widget.selected!);
+    }
+    updateValueAction();
+  }
+
+  void updateValueAction() {
+    if (widget.selected != null && widget.selected!.length != 0 && _value != widget.selected!) {
+      _value = widget.selected!;
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    updateValueAction();
+    List<Widget> list = widget.values.map((e) {
+      bool selected = e == _value;
+      return GestureDetector(
+        onTap: () {
+          selectedAction(e);
+          widget.onChanged(selectedValue);
+        },
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            selected
+                ? Icon(
+                    Icons.radio_button_checked,
+                    size: 16,
+                  )
+                : Icon(
+                    Icons.radio_button_off,
+                    size: 16,
+                  ),
+            Text(
+              e,
+              style: TextStyle(fontSize: 10),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+
+    return Wrap(
+      spacing: 10,
+      runSpacing: 5,
+      children: list,
+    );
+  }
+}
